c47c9177abc6a0d810d5a6cf3609ac3aa567e246
DjMarte <cristopher_marte@ucne.edu.do>
2025-01-29 20:22:48 -0400

    Agregando Hilt.

---
+++ app/src/main/java/edu/ucne/cristophermarte_p1_ap2/CristopherMarte_P1_AP2App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.cristophermarte_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CristopherMarte_P1_AP2App: Application() {
+}
---
+++ app/src/main/java/edu/ucne/cristophermarte_p1_ap2/data/di/AppModule.kt
@@ -0,0 +1,17 @@
+package edu.ucne.cristophermarte_p1_ap2.data.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideContexto(@ApplicationContext appContext: Context)  =  appContext
+}
