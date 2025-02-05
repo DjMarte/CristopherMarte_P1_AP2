b296d3bc9ec2f4b5bd30c1b56d68a0868e3cf8ee
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-05 18:34:35 -0400

    Renombrando Db

--- app/src/main/java/edu/ucne/cristophermarte_p1_ap2/data/local/database/CristopherMarteP1Db.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p1_ap2/data/local/database/CristopherMarteP1Db.kt
@@ -2,16 +2,16 @@ package edu.ucne.cristophermarte_p1_ap2.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import edu.ucne.cristophermarte_p1_ap2.data.local.dao.EntityDao
-import edu.ucne.cristophermarte_p1_ap2.data.local.entity.Entity
+import edu.ucne.cristophermarte_p1_ap2.data.local.dao.SistemaDao
+import edu.ucne.cristophermarte_p1_ap2.data.local.entity.Sistema
 
 @Database(
     entities = [
-        Entity::class,
+        Sistema::class,
     ],
     version = 1,
     exportSchema = false
 )
 abstract class CristopherMarteP1Db : RoomDatabase() {
-    abstract fun entityDao(): EntityDao
+    abstract fun entityDao(): SistemaDao
 }
