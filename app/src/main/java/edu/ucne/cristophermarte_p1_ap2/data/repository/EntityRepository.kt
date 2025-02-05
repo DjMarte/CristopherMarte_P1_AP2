a4ffa86d3cce629466e3275910aad24dadbd7e59
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-05 18:36:18 -0400

    Renombrando nombres en Repository.

--- app/src/main/java/edu/ucne/cristophermarte_p1_ap2/data/repository/EntityRepository.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p1_ap2/data/repository/EntityRepository.kt
@@ -1,18 +1,18 @@
 package edu.ucne.cristophermarte_p1_ap2.data.repository
 
-import edu.ucne.cristophermarte_p1_ap2.data.local.dao.EntityDao
-import edu.ucne.cristophermarte_p1_ap2.data.local.entity.Entity
+import edu.ucne.cristophermarte_p1_ap2.data.local.dao.SistemaDao
+import edu.ucne.cristophermarte_p1_ap2.data.local.entity.Sistema
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class EntityRepository @Inject constructor(
-    private val entityDao: EntityDao
+    private val sistemaDao: SistemaDao
 ) {
-    suspend fun saveEntity(entity: Entity) = entityDao.save(entity)
+    suspend fun saveEntity(sistema: Sistema) = sistemaDao.save(sistema)
 
-    suspend fun find(id: Int): Entity? =  entityDao.find(id)
+    suspend fun find(id: Int): Sistema? =  sistemaDao.find(id)
 
-    suspend fun delete(tecnico: Entity) =  entityDao.delete(tecnico)
+    suspend fun delete(sistema: Sistema) =  sistemaDao.delete(sistema)
 
-    fun getAll(): Flow<List<Entity>> = entityDao.getAll()
+    fun getAll(): Flow<List<Sistema>> = sistemaDao.getAll()
 }
