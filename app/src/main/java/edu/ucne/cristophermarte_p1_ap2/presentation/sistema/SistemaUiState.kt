f5d7b171b03b3d6778bc079ae50cba4ed355f132
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-05 19:12:21 -0400

    Creando la clase SistemaUiState.

---
+++ app/src/main/java/edu/ucne/cristophermarte_p1_ap2/presentation/sistema/SistemaUiState.kt
@@ -0,0 +1,10 @@
+package edu.ucne.cristophermarte_p1_ap2.presentation.sistema
+
+import edu.ucne.cristophermarte_p1_ap2.data.local.entity.SistemaEntity
+
+data class SistemaUiState(
+    val sistemaId: Int? = null,
+    val nombre: String = "",
+    val erroMessage: String? = null,
+    val listaSistemaEntity: List<SistemaEntity> = emptyList()
+)
