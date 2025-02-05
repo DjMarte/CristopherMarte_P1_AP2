778a15ddaed13c253a360c023060c733dd31b423
DjMarte <cristopher_marte@ucne.edu.do>
2025-02-05 19:13:23 -0400

    Agregando las rutas de navegación.

--- app/src/main/java/edu/ucne/cristophermarte_p1_ap2/navigation/ParcialNavHost.kt
+++ app/src/main/java/edu/ucne/cristophermarte_p1_ap2/navigation/ParcialNavHost.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import edu.ucne.cristophermarte_p1_ap2.presentation.HomeScreen
+import edu.ucne.cristophermarte_p1_ap2.presentation.sistema.SistemaListScreen
+import edu.ucne.cristophermarte_p1_ap2.presentation.sistema.SistemaScreen
 
 @Composable
 fun ParcialNavHost(
@@ -14,6 +18,26 @@ fun ParcialNavHost(
         startDestination = Screen.Home
     ) {
         composable<Screen.Home>{
+            HomeScreen(
+                goToSistemaList = {
+                    navHostController.navigate(Screen.SistemaListScreen)
+                }
+            )
+        }
+        composable<Screen.SistemaListScreen>{
+            SistemaListScreen(
+                goToAddSistema = { navHostController.navigate(Screen.Sistema(0)) },
+                goToEditSistema = { sistemaId ->
+                    navHostController.navigate(Screen.Sistema(sistemaId = sistemaId))
+                }
+            )
+        }
+        composable<Screen.Sistema>{
+            val sistemaId = it.toRoute<Screen.Sistema>().sistemaId
+            SistemaScreen(
+                sistemaId = sistemaId,
+                goBackToList = { navHostController.navigateUp() }
+            )
         }
     }
 }
